8a04def70b8bd6fe8f69bc69070934f03c9fbb06
AdeonMaster <adeonmaster@gmail.com>
2019-08-05 17:16:20 +0300

    Added more functions to User32 library defs

--- src/library_call_example.mjs
+++ src/library_call_example.mjs
@@ -5,3 +5,8 @@ const rect = new RECT();
 
 User32.GetWindowRect(HWND, rect.ref());
 console.log(rect);
+
+const styles = User32.GetWindowLongA(HWND, -16);
+console.log(styles);
+
+User32.SetCursorPos(512, 512);
--- src/windows/User32.mjs
+++ src/windows/User32.mjs
@@ -13,7 +13,9 @@ const RECT_Ptr = ref.refType(RECT);
 const User32 = ffi.Library('User32', {
   'GetForegroundWindow': [ 'int', [] ],
   'GetWindowRect': [ 'int', [ 'int', RECT_Ptr ]],
-  'SetForegroundWindow': [ 'int', ['int'] ]
+  'SetForegroundWindow': [ 'int', ['int'] ], 
+  'GetWindowLongA': [ 'int', ['int', 'int']], // https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getwindowlonga
+  'SetCursorPos': ['int', ['int', 'int']]
 });
 
 export default User32;
